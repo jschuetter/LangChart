dd86760b31e69c6dc9d9ce9b63859f2b7e82506e
jschuetter <100084569+jschuetter@users.noreply.github.com>
2024-05-13 16:15:11 -0400

    Second commit

--- src/Driver.java
+++ src/Driver.java
@@ -1,15 +1,37 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+import static java.nio.file.Files.newDirectoryStream;
 
 public class Driver extends JFrame implements ActionListener {
+    //Menu toolbar
     JMenuBar menuBar;
     JButton menuHome, menuOpen, menuCreate;
     final int MENU_ICON_W = 25, MENU_ICON_H = 25;
 
-    JPanel parentPnl, langSelect, chartSelect;
+    //Main view panel & layout
+    JPanel parentPnl;
+    CardLayout pnlLayout = new CardLayout();
+
+    //Language select panel
+    JPanel langPnl, langChoicePnl;
+    JButton newLangBtn;
+    ArrayList<JButton> langBtns;
+    //see PopulateLangPnl below
+
+    //Chart select panel
+    JPanel chartPnl;
+    //See PopulateChartPnl below
+
     JFileChooser filePicker;
     FileNameExtensionFilter filetypes = new FileNameExtensionFilter("Image files", "txt", "csv");
 
@@ -19,32 +41,44 @@ public class Driver extends JFrame implements ActionListener {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(700,500);
 
-        menuBar = new JMenuBar();
-        //menuBar.setLayout(new FlowLayout(FlowLayout.LEFT));
-        menuHome = new JButton();
-        menuHome.setIcon(getScaledIcon("resources/home.png", MENU_ICON_W, MENU_ICON_H));
-        menuHome.addActionListener(this);
-        menuBar.add(menuHome);
+        //Instantiate toolbar
+        {
+            menuBar = new JMenuBar();
+            //menuBar.setLayout(new FlowLayout(FlowLayout.LEFT));
+            menuHome = new JButton();
+            menuHome.setIcon(getScaledIcon("resources/home.png", MENU_ICON_W, MENU_ICON_H));
+            menuHome.addActionListener(this);
+            menuBar.add(menuHome);
 
-        menuOpen = new JButton();
-        menuOpen.setIcon(getScaledIcon("resources/open.png", MENU_ICON_W, MENU_ICON_H));
-        menuOpen.addActionListener(this);
-        menuBar.add(menuOpen);
+//        menuOpen = new JButton();
+//        menuOpen.setIcon(getScaledIcon("resources/open.png", MENU_ICON_W, MENU_ICON_H));
+//        menuOpen.addActionListener(this);
+//        menuBar.add(menuOpen);
 
-        menuCreate = new JButton();
-        menuCreate.setIcon(getScaledIcon("resources/create.png", MENU_ICON_W, MENU_ICON_H));
-        menuCreate.addActionListener(this);
-        menuBar.add(menuCreate);
+            menuCreate = new JButton();
+            menuCreate.setIcon(getScaledIcon("resources/create.png", MENU_ICON_W, MENU_ICON_H));
+            menuCreate.addActionListener(this);
+            menuBar.add(menuCreate);
 
-        this.add(menuBar, BorderLayout.NORTH);
+            this.add(menuBar, BorderLayout.NORTH);
+        }
 
-        parentPnl = new JPanel();
-        parentPnl.setLayout(new CardLayout());
-        langSelect = new JPanel();
+        //Instantiate panels
+        {
+            parentPnl = new JPanel();
+            parentPnl.setLayout(pnlLayout);
 
-        parentPnl.add(langSelect, "langSelect");
-        parentPnl.add(chartSelect, "chartSelect");
+            langPnl = new JPanel();
+            langPnl.setLayout(new BoxLayout(langPnl, BoxLayout.Y_AXIS));
+            PopulateLangPnl();
+            parentPnl.add(langPnl, "Langs");
 
+            chartPnl = new JPanel();
+            parentPnl.add(chartPnl, "Charts");
+
+            this.add(parentPnl, BorderLayout.CENTER);
+            pnlLayout.show(parentPnl, "Langs");
+        }
         this.setVisible(true);
     }
 
@@ -64,16 +98,35 @@ public class Driver extends JFrame implements ActionListener {
         Object src = e.getSource();
         if (src == menuHome) {
 
-        } else if (src == menuOpen) {
-
         } else if (src == menuCreate) {
+
+        } else if (src == newLangBtn) {
 
         }
     }
+
+    void PopulateLangPnl() {
+        langChoicePnl = new JPanel();
+        langChoicePnl.setLayout(new FlowLayout());
+        //ArrayList<String> langs = new ArrayList<>();
+        langBtns = new ArrayList<>();
+        Path chartsPath = FileSystems.getDefault().getPath("charts");
+        try (DirectoryStream<Path> langStream = newDirectoryStream(chartsPath)) {
+            for (Path langEntry: langStream) {
+                //langs.add(langEntry.getFileName().toString());
+                langBtns.add(new JButton(langEntry.getFileName().toString()));
+                langChoicePnl.add(langBtns.getLast());
+            }
+        } catch (IOException e) {
+            System.out.println("Directory read of dir 'charts' failed in PopulateLangPnl()");
+            //throw new RuntimeException(e);
+        }
+        langPnl.add(langChoicePnl);
+
+        newLangBtn = new JButton("Add Language");
+        newLangBtn.setIcon(getScaledIcon("resources/add.png", 10, 10));
+        //newLangBtn.setMaximumSize(newLangBtn.getPreferredSize());
+        newLangBtn.addActionListener(this);
+        langPnl.add(newLangBtn);
+    }
 }
-
-
-
-
-
-
